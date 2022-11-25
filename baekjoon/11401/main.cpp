fe86707d23304bba8ab4e42573a38dc6f01616e3
Jisu Sim <rlj1202@gmail.com>
2022-11-25 19:12:29 +0900

    2022-11-25

--- baekjoon/11401/main.cpp
+++ baekjoon/11401/main.cpp
@@ -1,15 +1,63 @@
-#include <cstdio>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-const long long MOD = 1e9 + 7;
+typedef long long ll;
+
+const ll MOD = 1e9 + 7;
+
+ll fact(ll n) {
+    ll result = 1;
+
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+        result %= MOD;
+    }
+
+    return result;
+}
+
+ll pow(ll n, ll k) {
+    ll result = 1;
+
+    ll remains = k;
+    ll cur = n;
+    while (remains) {
+        if (remains & 1) {
+            result *= cur;
+            result %= MOD;
+        }
+
+        remains >>= 1;
+        cur = cur * cur;
+        cur %= MOD;
+    }
+
+    return result;
+}
+
+ll inv(ll n) {
+    return pow(n, MOD - 2);
+}
+
+ll comb(ll n, ll k) {
+    ll result = fact(n);
+
+    result *= inv(fact(n - k));
+    result %= MOD;
+    result *= inv(fact(k));
+    result %= MOD;
+
+    return result;
+}
 
 int main() {
-    int N, K; // 4e6
-    scanf("%d %d", &N, &K);
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
+    ll N, K;
+    cin >> N >> K;
 
-    printf("%lld\n", 0LL);
+    cout << comb(N, K) << '\n';
 
     return 0;
 }
